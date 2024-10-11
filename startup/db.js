dea494f1cf2b8ade31a679cbe47dce4f3449dfc8
mohammadsaeed0901 <45822882+mohammadsaeed0901@users.noreply.github.com>
2024-10-11 15:29:39 +0330

    add options in db address

--- startup/db.js
+++ startup/db.js
@@ -4,6 +4,9 @@ const config = require("config");
 
 module.exports = function () {
     const db = config.get("db");
-    mongoose.connect(db)
+    mongoose.connect(config.get("db", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }))
     .then(() => winston.info(`Connected to ${db}...`));
 }
